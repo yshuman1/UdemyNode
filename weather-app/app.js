df6514ad2e609e888b08096aef8b20fb48c4211b
yshuman1 <yshuman@loyalloops.com>
2017-08-27 03:03:39 -0700

    commented out existing code in app.js to test out api call for darksky."

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,27 +1,37 @@
-const yargs = require('yargs');
+// const yargs = require('yargs');
 
-const geocode = require('./geocode/geocode.js');
+// const geocode = require('./geocode/geocode.js');
 
-const argv = yargs
-    .options({
-        a: {
-            demand:true,
-            alias: 'address',
-            describe: 'address to fetch weather for',
-            string: true
-        }
-})
-.help()
-.alias('help', 'h')
-.argv;
+// const argv = yargs
+//     .options({
+//         a: {
+//             demand:true,
+//             alias: 'address',
+//             describe: 'address to fetch weather for',
+//             string: true
+//         }
+// })
+// .help()
+// .alias('help', 'h')
+// .argv;
 
-geocode.geocodeAddress(argv.address, (errorMessage, results) => {
-    if (errorMessage) {
-        console.log(errorMessage);
-    } else {
-        console.log(JSON.stringify(results, undefined, 2));
+// geocode.geocodeAddress(argv.address, (errorMessage, results) => {
+//     if (errorMessage) {
+//         console.log(errorMessage);
+//     } else {
+//         console.log(JSON.stringify(results, undefined, 2));
         
-    }
+//     }
+// });
+
+const request = require('request');
+
+request({
+    url: `https://api.darksky.net/forecast/bda8dc00a416ee094927fd6b89a19565/37.9649458,-121.7232161`,
+    json: true
+}, (error, response, body) =>{
+    console.log(body.currently.temperature);
+    
 });
 
 
