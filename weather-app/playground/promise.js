0e10e4ab9c78f811f8198a1946dbec9b7bb3f0c2
yshuman1 <yshuman@loyalloops.com>
2017-09-01 01:20:25 -0700

    finished weather app. still confused a bit by promises...began working with express server

--- weather-app/playground/promise.js
+++ weather-app/playground/promise.js
@@ -10,17 +10,13 @@ var asyncAdd = (a,b) => {
     });
 };
 
-asyncAdd(5, 7).then((res) => {
+asyncAdd(5, '7').then((res) => {
 console.log('Result: ', res);  
 return asyncAdd(res, 33);  
-}, (errorMessage) => {
-    console.log(errorMessage);
 }).then((res) => {
-    console.log('should be 45', res);
-    
-}, (errorMessage) => {
+    console.log('should be 45: ', res);
+}).catch((errorMessage) => {
 console.log(errorMessage);
-
 });
 
 // var somePromise = new Promise((resolve, reject) => {
